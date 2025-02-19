fe70b8297498a0570408848cc52da64c7913fb59
Antonijela Gasic <antonijela_gasic@epam.com>
2025-02-19 23:14:07 +0100

    Added steps for verifying test cases page

--- cypress/e2e/test_case_7_verify_test_cases_page.cy.js
+++ cypress/e2e/test_case_7_verify_test_cases_page.cy.js
@@ -5,5 +5,9 @@ import { TestCases } from "../pages/test_cases_page"
 const test_cases = new TestCases()
 
 it('test cases', function (){
-    
+    cy.visit('')
+    homepage.getTestCasesOption().click()
+    cy.url().should('eq', 'https://automationexercise.com/test_cases')
+    test_cases.getTitle().should('have.text', 'Test Cases')
+    test_cases.getTestCasesList().should('be.visible')
 })
